19b42f22678ee6868e93e301bc5a6c5cd43955a1
Natsuserina <73052193+Natsuserina@users.noreply.github.com>
2021-05-07 01:25:56 +0900

    Update 巡回セールスマン.py

--- 巡回セールスマン.py
+++ 巡回セールスマン.py
@@ -6,6 +6,7 @@ for i in range(N):
 
 ALL = 1<<N
 
+#すでに訪れた都市の集合n, 最後にいる都市がiであるときのコストの最小値
 cost = []
 for n in range(ALL):
     cost.append([10**100]*N)
@@ -15,9 +16,11 @@ cost[0][0] = 0
 def bit(n, i):
     return (n & (1<<i)) > 0
 
+#すでに訪れた都市の集合n、現在いる都市i、これから向かう都市j
 for n in range(ALL):
     for i in range(N):
         for j in range(N):
+            #もしjをすでに訪れているか、現在地が都市jならcontinue
             if bit(n, j) or i == j:
                 continue
             
