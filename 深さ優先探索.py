146834c7479988621e438ed618eb351bfd43d1c1
Natsuserina <73052193+Natsuserina@users.noreply.github.com>
2021-05-19 00:34:45 +0900

    Update 深さ優先探索.py

--- 深さ優先探索.py
+++ 深さ優先探索.py
@@ -13,3 +13,15 @@ def dfs(i):
 
 dfs(i)
 
+#2次元グリッド
+def dfs(i, j):
+    visited[i][j] = True
+    for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+        i2 = i+di
+        j2 = j+dj
+        if not (0 <= i2 < H and 0 <= j2 < W):
+            continue
+        if not A[i2][j2]:
+            continue
+        if not visited[i2][j2]:
+            dfs(i2, j2)
