b995d7f6975323b20380e9d294e3b943f557bee5
Natsuserina <73052193+Natsuserina@users.noreply.github.com>
2021-05-20 21:06:55 +0900

    Update エラトステネスの篩.py

--- エラトステネスの篩.py
+++ エラトステネスの篩.py
@@ -1,8 +1,10 @@
 def eratosthenes(n):
-    prime = {i for i in range(2, n+1)}
-    for i in range(2, n+1):
-        if i in prime:
-            for j in range(i*2, n+1, i):
-                if j in prime:
-                    prime.remove(j)
-    return prime
+    is_prime = [True] * (n + 1)
+    is_prime[0] = False
+    is_prime[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if not is_prime[i]:
+            continue
+        for j in range(i * 2, n + 1, i):
+            is_prime[j] = False
+    return [i for i in range(n + 1) if is_prime[i]]
