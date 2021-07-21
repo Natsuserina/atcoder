5289fdf4b93728981167d1dae00ad682799f0a24
Natsuserina <73052193+Natsuserina@users.noreply.github.com>
2021-07-21 15:39:22 +0900

    Update ダイクストラ法.py

--- ダイクストラ法.py
+++ ダイクストラ法.py
@@ -1,36 +1,31 @@
 import heapq
 
 N, M = map(int, input().split())
+A = list(map(int, input().split()))
 G = []
 for i in range(N):
     G.append([])
 for i in range(M):
-    u, v, c = map(int, input().split())
-    G[u].append((c, v))
-    G[v].append((c, u))
+    u, v, t = map(int, input().split())
+    G[u].append((t, v))
+    G[v].append((t, u))
 
-dist = [-1]*N
-dist[0] = 0
-
+time = [-1]*N
+time[0] = 0
 done = [False]*N
-
 Q = []
-
 heapq.heappush(Q, (0, 0))
 
 while len(Q) > 0:
 
     d, i = heapq.heappop(Q)
-
     if done[i]:
         continue
-    
     done[i] = True
 
-    for (c, j) in G[i]:
-        if dist[j] == -1 or dist[j] > dist[i] + c:
-            dist[j] = dist[i] + c
-            heapq.heappush(Q, (dist[j], j))
+    for (t, j) in G[i]:
+        if time[j] == -1 or time[j] > time[i] + t:
+            time[j] = time[i] + t
+            heapq.heappush(Q, (time[j], j))
 
-print(dist[N-1])
-
+print(time[N-1])
