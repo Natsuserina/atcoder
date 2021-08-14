0113391167c8ac6595b2fe4c1f23c00b85c4f3bb
Natsuserina <73052193+Natsuserina@users.noreply.github.com>
2021-08-14 20:33:40 +0900

    Dijkstra.py

--- ダイクストラ法.py
+++ ダイクストラ法.py
@@ -1,31 +1,24 @@
 import heapq
 
 N, M = map(int, input().split())
-A = list(map(int, input().split()))
-G = []
-for i in range(N):
-    G.append([])
+G = [[] for i in range(N)]
 for i in range(M):
-    u, v, t = map(int, input().split())
-    G[u-1].append((t, v-1))
-    G[v-1].append((t, u-1))
+    a, b, c = map(int, input().split())
+    G[a-1].append((c, b-1))
+    G[b-1].append((c, a-1))
 
-time = [-1]*N
-time[0] = 0
-done = [False]*N
-Q = []
-heapq.heappush(Q, (0, 0))
-
-while len(Q) > 0:
-
-    d, i = heapq.heappop(Q)
-    if done[i]:
-        continue
-    done[i] = True
-
-    for (t, j) in G[i]:
-        if time[j] == -1 or time[j] > time[i] + t:
-            time[j] = time[i] + t
-            heapq.heappush(Q, (time[j], j))
-
-print(time[N-1])
+def Dijkstra(s):
+    dist = [-1]*N
+    done = [False]*N
+    Q = []
+    heapq.heappush(Q, (0, s))
+    dist[s] = 0
+    while len(Q) > 0:
+        d, i = heapq.heappop(Q)
+        if done[i]:
+            continue
+        for c, j in G[i]:
+            if dist[j] == -1 or dist[j] > dist[i] + c:
+                dist[j] = dist[i] + c
+                heapq.heappush(Q, (dist[j], j))
+    return dist
