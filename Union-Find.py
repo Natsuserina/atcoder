7533947e66fc02312aa6d89b0012f90bab1a317e
Natsuserina <73052193+Natsuserina@users.noreply.github.com>
2021-08-15 00:55:54 +0900

    Update Union-Find.py

--- Union-Find.py
+++ Union-Find.py
@@ -1,19 +1,18 @@
 N = int(input())
 
-#par[i]:頂点iの根。par[i]==iの時、自分がグループの根。
-par = [i for i in range(N)]
-#sz[i]:頂点iを部分木の根とみなしたときのその木の大きさ。
-sz = [1]*N
-ans = [0]*N
+#pair[i]:頂点iの根。par[i]==iの時、自分がグループの根。
+pair = [i for i in range(N)]
+#size[i]:頂点iを部分木の根とみなしたときのその木の大きさ。
+size = [1]*N
 
 def find(x):
     #根に到達したら、その頂点を返す
-    if par[x] == x:
+    if pair[x] == x:
         return x
     else:
         #経路圧縮
-        par[x] = find(par[x])
-        return par[x]
+        pair[x] = find(pair[x])
+        return pair[x]
 
 def union(x, y):
     rx, ry = find(x), find(y)
@@ -21,12 +20,12 @@ def union(x, y):
     if rx == ry:
         return
     #どちらの部分木が大きいか判断して、大きいほうの木に小さい木を併合する
-    if sz[rx] < sz[ry]:
-        sz[ry] += sz[rx]
-        par[rx] = ry
+    if size[rx] < size[ry]:
+        size[ry] += size[rx]
+        pair[rx] = ry
     else:
-        sz[rx] += sz[ry]
-        par[ry] = rx
+        size[rx] += size[ry]
+        pair[ry] = rx
 
 def same(x, y):
     return find(x) == find(y)
