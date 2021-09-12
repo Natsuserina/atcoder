4903eebeb6cb4eb691f56849b5b4ab927ddf1709
Natsuserina <73052193+Natsuserina@users.noreply.github.com>
2021-09-13 01:29:16 +0900

    Update 幅優先探索.py

--- 幅優先探索.py
+++ 幅優先探索.py
@@ -1,17 +1,21 @@
 from collections import deque
 #頂点数N、辺情報E、始点sは与えられている
-
-visited = [False]*N
+N, M = map(int(input().split()))
+G = [[] for i in range(N)]
+for i in range(M):
+    s, t = map(int, input().split())
+    G[s-1].append(t-1)
 
 Q = deque()
+dist = [-1]*N
 Q.append(s)
-visited[s] = True
+dist[s] = 0
 
 #キューから取り出しながら探索する
 while len(Q) > 0:
     i = Q.popleft()
-    for j in E[i]:
-        if not visited[j]:
-            visited[j] = True
+    for j in G[i]:
+        if dist[j] == -1:
+            dist[j] = dist[i] + 1
             Q.append(j)
             
